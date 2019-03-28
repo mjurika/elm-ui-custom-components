6d64b14821f2427368206fd12b64f0adc4f439f1
pes1899 <gulemitras@gmail.com>
2019-03-28 22:51:25 +0100

    Vyhldavanie nad slovenskom

--- src/Main.elm
+++ src/Main.elm
@@ -8,6 +8,8 @@ import CustomElement.MapView as MapView exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Decode exposing (Decoder, field, string)
 
 
 
@@ -33,6 +35,20 @@ type alias Model =
     , baseMap : String
     , triggerPosition : Int
     , position : Maybe Position
+    , inputText : String
+    , status : Status
+    }
+
+
+type Status
+    = Failure
+    | Loading
+    | Success (List SuggestResult)
+
+
+type alias SuggestResult =
+    { magicKey : String
+    , text : String
     }
 
 
@@ -53,6 +69,8 @@ init () =
       , baseMap = "satellite"
       , triggerPosition = 0
       , position = Nothing
+      , inputText = "bra"
+      , status = Loading
       }
     , Cmd.none
     )
@@ -67,6 +85,8 @@ type Msg
     | OnClick String
     | TriggerPosition
     | Position Position
+    | Search
+    | GotResult (Result Http.Error (List SuggestResult))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -89,6 +109,23 @@ update msg model =
             ( { model | position = Just value }
             , Cmd.none
             )
+
+        Search ->
+            ( { model | status = Loading }
+            , suggest model.inputText
+            )
+
+        GotResult result ->
+            case result of
+                Ok url ->
+                    ( { model | status = Success url }
+                    , Cmd.none
+                    )
+
+                Err _ ->
+                    ( { model | status = Failure }
+                    , Cmd.none
+                    )
 
 
 positionToString : Maybe Position -> String
@@ -113,45 +150,99 @@ positionToString position =
 view : Model -> Html Msg
 view model =
     div [ class "row" ]
-    [ div [ class "col s2" ]
-        [ h4 []
-            [ text "Elm custom elements"
+        [ div [ class "col s2" ]
+            [ h4 []
+                [ text "Elm custom elements"
+                ]
+            , DropdownButton.dropdownButton
+                [ DropdownButton.dropdownTitle model.title
+                , DropdownButton.dropdownItems model.items
+                , DropdownButton.onClick OnClick
+                ]
+                []
+            , div []
+                [ span []
+                    [ text "Selected basemap: " ]
+                , strong []
+                    [ text model.baseMap ]
+                ]
+            , br [] []
+            , button
+                [ class "btn"
+                , onClick TriggerPosition
+                ]
+                [ text "Show my position" ]
+            , GeoLocation.geoLocation
+                [ GeoLocation.triggerPosition model.triggerPosition
+                , GeoLocation.onPosition Position
+                ]
+                []
+            , div []
+                [ span []
+                    [ text "position: " ]
+                , strong []
+                    [ text <| positionToString model.position ]
+                ]
+            , div []
+                [ h2 [] [ text "Hladat" ]
+                , viewResponse model.status
+                ]
+            , button [ onClick Search ] [ text "Hladat" ]
             ]
-        , DropdownButton.dropdownButton
-            [ DropdownButton.dropdownTitle model.title
-            , DropdownButton.dropdownItems model.items
-            , DropdownButton.onClick OnClick
-            ]
-            []
-        , div []
-            [ span []
-                [ text "Selected basemap: " ]
-            , strong []
-                [ text model.baseMap ]
-            ]
-        , br [] []
-        , button
-            [ class "btn"
-            , onClick TriggerPosition
-            ]
-            [ text "Show my position" ]
-        , GeoLocation.geoLocation
-            [ GeoLocation.triggerPosition model.triggerPosition
-            , GeoLocation.onPosition Position
-            ]
-            []
-        , div []
-            [ span []
-                [ text "position: " ]
-            , strong []
-                [ text <| positionToString model.position ]
+        , div [ class "col s10" ]
+            [ MapView.mapView
+                [ MapView.baseMap model.baseMap
+                , MapView.position model.position
+                ]
+                []
             ]
         ]
-    , div [ class "col s10" ]
-        [ MapView.mapView
-            [ MapView.baseMap model.baseMap
-            , MapView.position model.position
-            ]
-            []
-        ]
-    ]
+
+
+viewResponse : Status -> Html Msg
+viewResponse status =
+    case status of
+        Failure ->
+            div []
+                [ text "error" ]
+
+        Loading ->
+            text "Loading..."
+
+        Success response ->
+            div []
+                [ text "podarilo sa" ]
+
+
+
+-- HTTP
+
+
+suggest : String -> Cmd Msg
+suggest query =
+    Http.get
+        { url = "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest?text=" ++ query ++ "&f=pjson&countryCode=SVK"
+        , expect = Http.expectJson GotResult suggestListDecoder
+        }
+
+
+
+-- https://developers.arcgis.com/rest/geocode/api-reference/geocoding-suggest.htm
+
+
+suggestListDecoder : Decoder (List SuggestResult)
+suggestListDecoder =
+    field "suggestions" (Decode.list suggestDecoder)
+
+
+suggestDecoder : Decoder SuggestResult
+suggestDecoder =
+    Decode.map2 SuggestResult
+        (Decode.field "magicKey" Decode.string)
+        (Decode.field "text" Decode.string)
+
+
+
+-- suggestDecoder : Decoder String
+-- suggestDecoder =
+--     field "data" (field "url" string)
